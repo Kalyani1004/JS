bc2fc48ad1405098fd0253174fa74f6e8f3d156f
Kalyani <kalyanibharsat123@gmail.com>
2024-06-21 22:15:00 +0530

    Add all files

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accId = 101
+let accEmail = "kalyanibharsat@gmail.com"
+var pwd ="12345"
+let accountCity;
+
+// accId = 102 
+console.log(accId);
+
+console.table([accEmail, accId, pwd,accountCity])
+
+//dont use var because of block scope and function scope
+// jr var name eka yachyat use kelay tr dusrikde pn use kela asel tithe pn change hoil
---
+++ 02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3+3) using node.js not browser if you want answer then type it on console
+console.log(3+3)
+console.log("kalyani")
+
+let name ="Kalyani" //string
+let age =21 //int
+let isloggedin = false //boolean
+
+
+//primitive data types
+//number => 2 to power 53
+//bigint
+//boolean => true/false
+//null => standalone value (khali), empty , it is an object 
+//undefined => value is not assigned
+//symbol => unique
+
+
+//objects 
+console.log(typeof null); //object
